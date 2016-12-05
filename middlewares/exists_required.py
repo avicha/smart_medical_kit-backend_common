bd2ff64636ca0e2d0a7bdd46756742648c6d974b
avicha <avichabc@gmail.com>
2016-12-05 19:03:15 +0800

    feat:增加了user_address的controller和exists中间件

--- middlewares/exists_required.py
+++ middlewares/exists_required.py
@@ -18,3 +18,21 @@ def user_exists(f):
         else:
             raise UserModel.LackOfFieldError(u'请传入用户ID')
     return decorator
+
+
+def user_address_exists(f):
+    from backend_common.models.user_address import UserAddress as UserAddressModel
+
+    def decorator(*args, **kwargs):
+        data = request.json or request.form or request.args
+        user_address_id = data.get('user_address_id')
+        if user_address_id:
+            try:
+                user_address = UserAddressModel.get(UserAddressModel.id == user_address_id, UserAddressModel.deleted_at == None)
+                kwargs.update({'user_address': user_address})
+                return f(*args, **kwargs)
+            except UserAddressModel.DoesNotExist as e:
+                raise UserAddressModel.NotFoundError(u'该用户地址不存在')
+        else:
+            raise UserAddressModel.LackOfFieldError(u'请传入用户地址ID')
+    return decorator
