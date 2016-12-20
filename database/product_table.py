b69df0e2c1e3bb17f39f2ab4d78baba5b97098cc
avicha <avichabc@gmail.com>
2016-12-20 17:48:20 +0800

    feat:增加了product_instance的controller

--- database/product_table.py
+++ database/product_table.py
@@ -6,4 +6,4 @@ def migrate():
     if Product.table_exists():
         Product.drop_table()
     Product.create_table()
-    Product.create(name='Pill Bot智能药盒', description='这是一款方便老人家药物管理的药盒', images='', intro='<p>这是一款方便老人家药物管理的药盒</p>', props='大小:30cm*20cm*20cm;适用人群:18-60岁;')
+    Product.create(name='Pill Bot智能药盒', description='这是一款方便老人家药物管理的药盒', images='', intro='<p>这是一款方便老人家药物管理的药盒</p>', props='大小:30cm*20cm*20cm;适用人群:18-60岁')
