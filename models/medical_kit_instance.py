3752cb3fe33cde786b2c73a8fbbfbb8b0adf4400
avicha <avichabc@gmail.com>
2017-01-06 15:44:13 +0800

    feat:增加了药箱设置的controller

--- models/medical_kit_instance.py
+++ models/medical_kit_instance.py
@@ -11,3 +11,16 @@ class MedicalKitInstance(BaseModel):
 
     class Meta:
         db_table = 'medical_kit_instances'
+
+    def setting(self, fields=['id', 'prompt_sound']):
+        from medical_kit_instance_setting import MedicalKitInstanceSetting as MedicalKitInstanceSettingModel
+        setting = MedicalKitInstanceSettingModel.select().where(MedicalKitInstanceSettingModel.medical_kit_instance_id == self.id, MedicalKitInstanceSettingModel.deleted_at == None).first()
+        return setting.format(fields)
+
+    def box_settings(setting, fields=['id', 'box_index', 'medical_name', 'medical_barcode', 'schedule_times', 'piece_per_time', 'unit']):
+        from medical_kit_instance_box_setting import MedicalKitInstanceBoxSetting as MedicalKitInstanceBoxSettingModel
+        q = MedicalKitInstanceBoxSettingModel.select().where(MedicalKitInstanceBoxSettingModel.medical_kit_instance_id == self.id, MedicalKitInstanceBoxSettingModel.deleted_at == None)
+        settings = []
+        for x in q:
+            settings.append(x.format(fields))
+        return settings
